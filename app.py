e8ff12fc0487668fe00d44cba3d0c818d5d9650b
beratavseren <130407401+beratavseren@users.noreply.github.com>
2024-11-07 23:05:38 +0300

    post isteği ile çarpma işlemi
    post isteği ile '/carpim/<int:sayi1>*<int:sayi2>' şeklinde gönderilen isteklere iki sayının çarpımını geri döndüren endpoint eklendi

--- app.py
+++ app.py
@@ -9,9 +9,13 @@ class Toplam(Resource):
     def get(self, sayi1, sayi2):
         return {'toplam': sayi1+sayi2}
 
+class Carpim(Resource):
+    def post(self, sayi1, sayi2):
+        return {'carpim': sayi1*sayi2}
+
 
 api.add_resource(Toplam, '/toplam/<int:sayi1>+<int:sayi2>')
-
+api.add_resource(Carpim, '/carpim/<int:sayi1>*<int:sayi2>')
 
 if __name__ == '__main__':
     app.run()
