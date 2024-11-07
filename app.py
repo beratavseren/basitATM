76425c7c05a188adc213ab4200e6bee2a42e5bee
beratavseren <130407401+beratavseren@users.noreply.github.com>
2024-11-07 23:08:07 +0300

    port değişikliği
    rest api'nin dinlediği port Adıyaman ilinin plaka numarası olan 2 ile değiştirildi

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ class Toplam(Resource):
     def get(self, sayi1, sayi2):
         return {'toplam': sayi1+sayi2}
 
+
 class Carpim(Resource):
     def post(self, sayi1, sayi2):
         return {'carpim': sayi1*sayi2}
@@ -18,4 +19,4 @@ api.add_resource(Toplam, '/toplam/<int:sayi1>+<int:sayi2>')
 api.add_resource(Carpim, '/carpim/<int:sayi1>*<int:sayi2>')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=2)
